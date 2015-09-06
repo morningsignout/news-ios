2cd30d6d7064a35d472eb79314aa5545aecf72dc
shannonphu <shannonphu@sbcglobal.net>
2015-09-06 12:45:50 -0700

    add tap gesture recognizers on tiles; prepped for full article reading view

---
+++ ios-app/FullPostViewController.h
@@ -0,0 +1,13 @@
+//
+//  FullPostViewController.h
+//  ios-app
+//
+//  Created by Shannon Phu on 9/6/15.
+//  Copyright (c) 2015 Morning Sign Out Incorporated. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FullPostViewController : UIViewController
+
+@end
---
+++ ios-app/FullPostViewController.m
@@ -0,0 +1,37 @@
+//
+//  FullPostViewController.m
+//  ios-app
+//
+//  Created by Shannon Phu on 9/6/15.
+//  Copyright (c) 2015 Morning Sign Out Incorporated. All rights reserved.
+//
+
+#import "FullPostViewController.h"
+
+@interface FullPostViewController ()
+
+@end
+
+@implementation FullPostViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
