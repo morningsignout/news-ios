f496921d6c5b742f1db611f338fee650ec2b3566
QINGWEI LAN <qingweilan@gmail.com>
2015-11-08 19:37:03 -0800

    Created 'CoreDataControl' & moved core data stack from 'AppDelegate' to 'CoreDataControl'

---
+++ ios-app/CoreDataLibrary/CoreDataControl.h
@@ -0,0 +1,21 @@
+//
+//  CoreDataControl.h
+//  ios-app
+//
+//  Created by Qingwei Lan on 11/8/15.
+//  Copyright © 2015 Morning Sign Out Incorporated. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+@interface CoreDataControl : NSObject
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
