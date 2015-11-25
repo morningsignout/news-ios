2a50ebd949c6ba6e89f670bbe2c71f24a39a6dae
shannonphu <shannonphu@sbcglobal.net>
2015-11-25 00:37:29 -0800

    fix category view autolayout issues and subscriptions view

--- ios-app/CategoryViewController.m
+++ ios-app/CategoryViewController.m
@@ -15,6 +15,7 @@
 
 @property (strong, nonatomic) NSArray *categoryNames;
 @property (weak, nonatomic) IBOutlet UIView *containerView;
+@property (weak, nonatomic) IBOutlet UIView *buttonView;
 
 @end
 
@@ -25,14 +26,6 @@
     // Do any additional setup after loading the view.
     self.navigationItem.title = @"Categories";
     
-    UISwipeGestureRecognizer *swipeDown = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(moveContainerDown)];
-    [swipeDown setDirection:UISwipeGestureRecognizerDirectionDown];
-    
-    UISwipeGestureRecognizer *swipeUp = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(moveContainerUp)];
-    [swipeUp setDirection:UISwipeGestureRecognizerDirectionUp];
-    
-    [self.containerView addGestureRecognizer:swipeDown];
-    [self.containerView addGestureRecognizer:swipeUp];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
@@ -41,25 +34,22 @@
     self.navigationController.navigationBarHidden = YES;
 }
 
-- (void)viewDidAppear:(BOOL)animated {
-    [self moveContainerDown];
-}
-
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
-- (void)moveContainerDown {
-    [UIView animateWithDuration:1.0 animations:^{
-        self.containerView.frame = CGRectMake(0, 9 * self.view.frame.size.height / 10, self.view.frame.size.width, self.view.frame.size.height);
+- (IBAction)showSubscriptions:(id)sender {
+    [UIView animateWithDuration:0.5 animations:^{
+        self.containerView.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height);
     }];
 }
 
-- (void)moveContainerUp {
-    [UIView animateWithDuration:1.0 animations:^{
-        self.containerView.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height);
+- (IBAction)showCategories:(id)sender {
+    [UIView animateWithDuration:0.5 animations:^{
+        self.containerView.frame = CGRectMake(0, self.view.frame.size.height, self.view.frame.size.width, self.view.frame.size.height);
     }];
+
 }
 
 #pragma mark - Navigation
